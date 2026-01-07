9589243f9d36da730a9baf235900d50fc6a10a66
ghifaricode <alghifarifaiz45@gmail.com>
2026-01-07 11:07:29 +0700

    Pembaruan Setelah Presentasi

--- app/src/main/java/com/example/salaryprediction/api/ApiClient.kt
+++ app/src/main/java/com/example/salaryprediction/api/ApiClient.kt
@@ -5,18 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
 
-    // GANTI baseUrl sesuai lingkungan kamu
-
-    // Kalau pakai EMULATOR Android:
-    // Flask di laptop: http://localhost:5000
-    // Base URL di Android:
-    // http://10.0.2.2:5000/
-    //
-    // Kalau pakai HP FISIK:
-    // - Cari IP laptop kamu, misal: 192.168.0.10
-    // - Base URL: "http://192.168.0.10:5000/"
-
-    private const val BASE_URL = "https://myreport12-salary-api.hf.space/"
+    private const val BASE_URL = "http://10.0.2.2:5000/"
 
     val apiService: SalaryApiService by lazy {
         Retrofit.Builder()
